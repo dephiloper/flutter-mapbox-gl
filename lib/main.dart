333d570632c86b619f756dd72ab19662020ee76a
dephiloper <s0554213@htw-berlin.de>
2019-11-10 21:46:17 +0100

    for testing on ios

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+String _customStyle;
+Future<String> _loadJson() async {
+  return await rootBundle.loadString('assets/style.json');
+}
+
+void main() async {
+  _customStyle = await _loadJson();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MapWidget());
+  }
+}
+
+class MapWidget extends StatefulWidget {
+  @override
+  _MapWidgetState createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  final CameraPosition _kInitialPosition;
+  final CameraTargetBounds _cameraTargetBounds;
+  static double defaultZoom = 12.0;
+
+  CameraPosition _position;
+  MapboxMapController mapController;
+  bool _isMoving = false;
+  bool _compassEnabled = true;
+  MinMaxZoomPreference _minMaxZoomPreference =
+  const MinMaxZoomPreference(6.0, 20.0);
+  String _styleString = "mapbox://styles/mapbox/outdoors-v11";
+  bool _rotateGesturesEnabled = true;
+  bool _scrollGesturesEnabled = true;
+  bool _tiltGesturesEnabled = false;
+  bool _zoomGesturesEnabled = true;
+  bool _myLocationEnabled = false;
+  MyLocationTrackingMode _myLocationTrackingMode = MyLocationTrackingMode.None;
+
+  _MapWidgetState._(this._kInitialPosition, this._position, this._cameraTargetBounds);
+
+  static CameraPosition _getCameraPosition() {
+    final latLng = LatLng(52.520008, 13.404954);
+    return CameraPosition(
+      target: latLng,
+      zoom: defaultZoom,
+    );
+  }
+
+  factory _MapWidgetState() {
+    CameraPosition cameraPosition = _getCameraPosition();
+
+    final cityBounds = LatLngBounds(
+      southwest: LatLng(52.33826, 13.08835),
+      northeast: LatLng(52.67551, 13.76116),
+    );
+
+    return _MapWidgetState._(
+        cameraPosition, cameraPosition, CameraTargetBounds(cityBounds));
+  }
+
+  void _onMapChanged() {
+    setState(() {
+      _extractMapInfo();
+    });
+  }
+
+  @override
+  void dispose() {
+    if (mapController != null) {
+      mapController.removeListener(_onMapChanged);
+    }
+    super.dispose();
+  }
+
+  void _extractMapInfo() {
+    _position = mapController.cameraPosition;
+    _isMoving = mapController.isCameraMoving;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: _buildMapBox(context),
+    );
+  }
+
+  MapboxMap _buildMapBox(BuildContext context) {
+    return MapboxMap(
+          //       "tiles": ["https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt"],
+        onMapCreated: onMapCreated,
+        initialCameraPosition: this._kInitialPosition,
+        trackCameraPosition: true,
+        compassEnabled: _compassEnabled,
+        cameraTargetBounds: _cameraTargetBounds,
+        minMaxZoomPreference: _minMaxZoomPreference,
+        styleString: _customStyle,//_styleString,
+        rotateGesturesEnabled: _rotateGesturesEnabled,
+        scrollGesturesEnabled: _scrollGesturesEnabled,
+        tiltGesturesEnabled: _tiltGesturesEnabled,
+        zoomGesturesEnabled: _zoomGesturesEnabled,
+        myLocationEnabled: _myLocationEnabled,
+        myLocationTrackingMode: _myLocationTrackingMode,
+        onCameraTrackingDismissed: () {
+          this.setState(() {
+            _myLocationTrackingMode = MyLocationTrackingMode.None;
+          });
+        });
+  }
+
+  void onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+    mapController.addListener(_onMapChanged);
+    _extractMapInfo();
+    setState(() {});
+  }
+}
